6c223c63125652b6f1091744045b280df38ed03a
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-02-19 19:02:54 +0800

    增加根据ID获取单个帖子的API

---
+++ Controllers/PostMetadataController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TangyuanBackendASP.Data;
+using TangyuanBackendASP.Models;
+
+namespace TangyuanBackendASP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostMetadataController : Controller
+    {
+        private readonly TangyuanDbContext _db;
+
+        public PostMetadataController(TangyuanDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public PostMetadata GetSingle(int postId)
+        {
+            return _db.PostMetadata.Where(p => p.PostId == postId).FirstOrDefault();
+        }
+    }
+}
