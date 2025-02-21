88f6b1cf5261dd4640e287ee32a4829b1c3d1da3
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-02-21 10:02:52 +0800

    新增User模型

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TangyuanBackendASP.Models
+{
+    [PrimaryKey(nameof(UserId))]
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string NickName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+}
