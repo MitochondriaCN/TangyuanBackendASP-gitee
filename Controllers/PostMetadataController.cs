2559e5726000fe8e36e139acdb7d569b152dfd7c
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-02-23 18:10:53 +0800

    添加帖子元数据的 CRUD API 方法
    在 `PostMetadataController.cs` 文件中，新增了获取单个帖子元数据、创建新帖子和删除帖子的 API 方法。具体更改包括：
    - 添加 `GetSingle` 方法，根据帖子 ID 获取单个帖子元数据。
    - 添加 `Post` 方法，创建新的帖子元数据并自动生成有效的帖子 ID。
    - 添加 `Delete` 方法，根据帖子 ID 删除指定的帖子元数据。
    - 引入 `CreatPostMetadataDto` 类，用于接收创建帖子时所需的数据。

--- Controllers/PostMetadataController.cs
+++ Controllers/PostMetadataController.cs
@@ -15,10 +15,56 @@ namespace TangyuanBackendASP.Controllers
             _db = db;
         }
 
+        //查
         [HttpGet("{id}")]
         public PostMetadata GetSingle(int id)
         {
             return _db.PostMetadata.Where(p => p.PostId == id).FirstOrDefault();
+        }
+
+        //增
+        [HttpPost]
+        public IActionResult Post([FromBody] CreatPostMetadataDto post)
+        {
+            PostMetadata maxIdPost = _db.PostMetadata.OrderByDescending(p => p.PostId).FirstOrDefault();
+            int validId = maxIdPost == null ? 1 : maxIdPost.PostId + 1;
+            PostMetadata p = new()
+            {
+                PostId = validId,
+                UserId = post.UserId,
+                PostDateTime = post.PostDateTime,
+                SectionId = post.SectionId,
+                Title = post.Title,
+                IsVisible = post.IsVisible
+            };
+            _db.PostMetadata.Add(p);
+            _db.SaveChanges();
+            return Ok();
+        }
+
+        //删
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            PostMetadata p = _db.PostMetadata.Where(p => p.PostId == id).FirstOrDefault();
+            if (p == null)
+            {
+                return NotFound();
+            }
+            _db.PostMetadata.Remove(p);
+            _db.SaveChanges();
+            return Ok();
+        }
+
+        //我们姑且认为推文是不需要提供修改API的
+
+        public class CreatPostMetadataDto
+        {
+            public int UserId { get; set; }
+            public DateTime PostDateTime { get; set; }
+            public int SectionId { get; set; }
+            public string Title { get; set; }
+            public bool IsVisible { get; set; }
         }
     }
 }
