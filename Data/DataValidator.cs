023e1bfd69339d117065c693b15c333a05311b29
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-02-24 10:17:47 +0800

    添加电话号码验证功能 在 `DataValidator.cs` 文件中新增 `DataValidator` 类和 `CountryCode` 枚举。`CountryCode` 枚举定义了中国、美国、印度和英国的国家代码。`IsPhoneNumberValid` 方法用于根据国家代码验证电话号码的合法性，使用正则表达式进行格式检查，并在不支持的国家代码时抛出异常。

---
+++ Data/DataValidator.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace TangyuanBackendASP.Data
+{
+    public enum CountryCode
+    {
+        China,
+        UnitedStates,
+        India,
+        UnitedKingdom
+    }
+
+    public static class DataValidator
+    {
+        /// <summary>
+        /// 判断电话号码是否合法，支持的国家在<see cref="CountryCode"/>枚举中定义。
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <param name="countryCode"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
+        public static bool IsPhoneNumberValid(string phoneNumber, CountryCode countryCode)
+        {
+            // 根据国家码选择相应的正则表达式
+            string pattern = countryCode switch
+            {
+                CountryCode.China => @"^(\+?0?86\-?)?1[345789]\d{9}$", // 中国
+                CountryCode.UnitedStates => @"^\+?1?\d{10}$", // 美国
+                CountryCode.India => @"^\+?91?\d{10}$", // 印度
+                _ => throw new NotSupportedException("不支持的国家码")
+            };
+
+            // 使用正则表达式判断电话号码是否合法
+            return Regex.IsMatch(phoneNumber, pattern);
+        }
+    }
+}
