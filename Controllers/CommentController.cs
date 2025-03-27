b60cc44adbe5fdf82e7d94e6ab26dc418a017418
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-03-27 10:39:43 +0800

    更新评论模型和控制器，添加 API 功能 在 `Comment.cs` 中引入 EF Core，并将 `ParentCommentId` 类型更改为 `int`。 在 `CommentController.cs` 中添加多个 API 方法，包括获取、删除和创建评论的功能，并实现了相应的 DTO 类和授权处理。

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TangyuanBackendASP.Data;
+using TangyuanBackendASP.Models;
+
+namespace TangyuanBackendASP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommentController : Controller
+    {
+
+        private TangyuanDbContext _db;
+
+        public CommentController(TangyuanDbContext db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// 查
+        /// </summary>
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            Comment c = _db.Comment.Where<Comment>(c => c.CommentId == id).FirstOrDefault();
+            if (c == null)
+            {
+                return NotFound("No such comment.");
+            }
+            else
+            {
+                return Ok(new Comment()
+                {
+                    CommentId = c.CommentId,
+                    ParentCommentId = c.ParentCommentId,
+                    UserId = c.UserId,
+                    PostId = c.PostId,
+                    Content = c.Content,
+                    CommentDateTime = c.CommentDateTime
+                });
+            }
+        }
+
+        /// <summary>
+        /// 删
+        /// </summary>
+        [Authorize]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Comment c = _db.Comment.Where<Comment>(c => c.CommentId == id).FirstOrDefault();
+            if (c == null)
+            {
+                return NotFound("No such comment.");
+            }
+            else
+            {
+                _db.Comment.Remove(c);
+                _db.SaveChanges();
+                return Ok("Comment deleted.");
+            }
+        }
+
+        /// <summary>
+        /// 增
+        /// </summary>
+        [Authorize]
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateCommentDto comment)
+        {
+            Comment maxIdComment = _db.Comment.OrderByDescending(c => c.CommentId).FirstOrDefault();
+            Comment c = new()
+            {
+                CommentId = maxIdComment == null ? 1 : maxIdComment.CommentId + 1,
+                ParentCommentId = comment.ParentCommentId,
+                UserId = comment.UserId,
+                PostId = comment.PostId,
+                Content = comment.Content,
+                CommentDateTime = comment.CommentDateTime
+            };
+            _db.Comment.Add(c);
+            _db.SaveChanges();
+            return Ok(new
+            {
+                CommentId = c.CommentId
+            });
+        }
+
+        public class CreateCommentDto
+        {
+            public required string ParentCommentId { get; set; }
+            public required int UserId { get; set; }
+            public required int PostId { get; set; }
+            public required string Content { get; set; }
+            public required DateTime CommentDateTime { get; set; }
+        }
+    }
+}
--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,9 +1,12 @@
-﻿namespace TangyuanBackendASP.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TangyuanBackendASP.Models
 {
+    [PrimaryKey(nameof(CommentId))]
     public class Comment
     {
         public required int CommentId { get; set; }
-        public required string ParentCommentId { get; set; }
+        public required int ParentCommentId { get; set; }
         public required int UserId { get; set; }
         public required int PostId { get; set; }
         public required string Content { get; set; }
