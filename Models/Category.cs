4360fa350c6493e2b5ebc77250804c8bd5f3f4e5
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-04-09 13:03:24 +0800

    添加 Category 类及其属性 在 `Category.cs` 文件中，新增了命名空间 `TangyuanBackendASP.Models`，并定义了 `Category` 类。该类包含三个必需的属性：`CategoryId`、`BaseName` 和 `BaseDescription`，后两者的默认值为空字符串。类的注释详细说明了模型的用途及属性的语言特性。

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace TangyuanBackendASP.Models
+{
+    /// <summary>
+    /// 领域模型。所有以Base开头的属性都是中文的，作为默认值。
+    /// </summary>
+    public class Category
+    {
+        public required int CategoryId { get; set; }
+        public required string BaseName { get; set; } = string.Empty;
+        public required string BaseDescription { get; set; } = string.Empty;
+    }
+}
