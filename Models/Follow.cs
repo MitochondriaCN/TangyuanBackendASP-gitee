236091c5b1ebfe2109732140a69b8d8a9c96b9e8
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-08-10 10:44:29 +0800

    添加 Follow 类及其属性
    在 `Follow.cs` 文件中，添加了 `Microsoft.EntityFrameworkCore` 命名空间，并定义了一个名为 `Follow` 的类。该类使用了 `PrimaryKey` 特性，指定 `SourceUserId` 和 `TargetUserId` 作为主键。类中包含三个属性：`SourceUserId`、`TargetUserId` 和 `FollowedAt`，其中 `SourceUserId` 和 `TargetUserId` 是必需的整数类型，`FollowedAt` 是必需的日期时间类型。

---
+++ Models/Follow.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TangyuanBackendASP.Models
+{
+    [PrimaryKey(nameof(SourceUserId), nameof(TargetUserId))]
+    public class Follow
+    {
+        public required int SourceUserId { get; set; }
+        public required int TargetUserId { get; set; }
+        public required DateTime FollowedAt { get; set; } 
+    }
+}
